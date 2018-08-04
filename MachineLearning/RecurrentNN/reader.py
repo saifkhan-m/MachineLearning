6ef1dc15219f1289c46053e92658cce209d7b8df
Saif Khan <Saif Khan>
2018-08-04 17:09:34 +0530

    Added the implementation for Recurrent Neural Nets and bug fix in the reader.

--- MachineLearning/RecurrentNN/reader.py
+++ MachineLearning/RecurrentNN/reader.py
@@ -28,18 +28,6 @@ def _file_to_word_ids(filename, word_to_id):
 
 
 def ptb_raw_data(data_path=None):
-    """Load PTB raw data from data directory "data_path".
-    Reads PTB text files, converts strings to integer ids,
-    and performs mini-batching of the inputs.
-    The PTB dataset comes from Tomas Mikolov's webpage:
-    http://www.fit.vutbr.cz/~imikolov/rnnlm/simple-examples.tgz
-    Args:
-      data_path: string path to the directory where simple-examples.tgz
-                 has been extracted.
-    Returns:
-      tuple (train_data, valid_data, test_data, vocabulary)
-      where each of the data objects can be passed to PTBIterator.
-    """
 
     train_path = os.path.join(data_path, "ptb.train.txt")
     valid_path = os.path.join(data_path, "ptb.valid.txt")
@@ -54,22 +42,7 @@ def ptb_raw_data(data_path=None):
 
 
 def ptb_producer(raw_data, batch_size, num_steps, name=None):
-    """Iterate on the raw PTB data.
-    This chunks up raw_data into batches of examples and returns
-    Tensors that are drawn from these batches.
-    Args:
-      raw_data: one of the raw data outputs from ptb_raw_data.
-      batch_size: int, the batch size.
-      num_steps: int, the number of unrolls.
-      name: the name of this operation (optional).
-    Returns:
-      A pair of Tensors, each shaped [batch_size, num_steps]. The
-      second element of the tuple is the same data time-shifted to the
-      right by one.
-    Raises:
-      tf.errors.InvalidArgumentError: if batch_size or num_steps are
-      too high.
-    """
+
     with tf.name_scope(name, "PTBProducer",
                        [raw_data, batch_size, num_steps]):
         raw_data = tf.convert_to_tensor(raw_data, name="raw_data",
